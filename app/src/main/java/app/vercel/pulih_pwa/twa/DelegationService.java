8d1bb7ec820ef26e346d98a366ef8515ad807d21
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-11-12 19:11:53 +0700

    feat : release apk

---
+++ app/src/main/java/app/vercel/pulih_pwa/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.pulih_pwa.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
